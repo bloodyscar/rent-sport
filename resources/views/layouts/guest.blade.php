f9beca926160479560554d99dbc44db6f06cf375
Adit H <adit.hernowo@gmail.com>
2025-01-07 15:55:49 +0700

    asd

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased" >
-        <div class="min-h-screen flex flex-col sm:justify-center items-center items-center justify-center pt-6 sm:pt-0 bg-gray-500"  style="background-image: url('http://localhost:8000/storage/public/trafold.jpg'); background-size: cover; background-position: center;">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center items-center justify-center pt-6 sm:pt-0 bg-gray-500"  style="background-image: url('http://localhost:8000/storage/images/trafold.jpg'); background-size: cover; background-position: center;">
             
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg" >
                 {{ $slot }}
